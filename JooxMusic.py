c513830c64455e979b09cc9ce4091cf1eebaee54
herywinarto <hery.winarto@binus.ac.id>
2020-03-08 06:30:02 +0700

    Create JooxMusic.py

---
+++ JooxMusic.py
@@ -0,0 +1,18 @@
+import requests, json
+
+### QR ROTATE LOGIN EXAMPLE ###
+
+token_key= "INSERT TOKEN KEY HERE"
+## GET API KEY FROM https://api.boteater.us/get_token ###
+
+def search():
+    search = input("Search query: ") #example chainsmokers
+    result = json.loads(requests.get("https://api.boteater.us/joox?search="+search+"&auth="+token_key).text)
+    for music in result["result"]:
+        print("")
+        print("Artis: "+music["artist"])
+        print("Title: "+music["title"])
+        print("Link Audio: "+music["stream-url"])
+        print("Link Image: "+music["image"])
+
+search()
