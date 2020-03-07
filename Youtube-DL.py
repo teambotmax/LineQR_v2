46c7b07df98404b1d52da0c935d4d1838a0c6ec1
herywinarto <hery.winarto@binus.ac.id>
2020-03-08 06:50:16 +0700

    Update Youtube-DL.py

--- Youtube-DL.py
+++ Youtube-DL.py
@@ -5,7 +5,8 @@ import requests, json
 token_key= "INSERT TOKEN KEY HERE"
 ## GET API KEY FROM https://api.boteater.us/get_token ###
 
-def ytSearch(search):
+def ytSearch():
+    search= input("Search query: ") #example joji cant get over you
     result = json.loads(requests.get("https://api.boteater.us/ytdl?search="+search+"&auth="+token_key).text)
     print("Title: "+result["result"]["title"])
     print("Artist: "+result["result"]["artist"])
@@ -13,10 +14,14 @@ def ytSearch(search):
     print("Video Download: "+result["result"]["video_url"])
     print("Audio Download: "+result["result"]["audio_url"])
 
-def ytLink(link):
-    result = json.loads(requests.get("https://api.boteater.us/ytdl?url="+link+"&auth="+token_key).text)
+def ytLink():
+    url= input("Link Youtube: ") #example https://www.youtube.com/watch?v=zbxAB7rTpDc
+    result = json.loads(requests.get("https://api.boteater.us/ytdl?url="+url+"&auth="+token_key).text)
     print("Title: "+result["result"]["title"])
     print("Artist: "+result["result"]["artist"])
     print("Thumbnail: "+result["result"]["thumbnail"])
     print("Video Download: "+result["result"]["video_url"])
     print("Audio Download: "+result["result"]["audio_url"])
+
+
+ytLink()
