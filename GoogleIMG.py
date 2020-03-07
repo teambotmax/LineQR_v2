cb3c0660a0eaccddaf107cb12871d7cb4cf2d127
herywinarto <hery.winarto@binus.ac.id>
2020-03-08 06:12:28 +0700

    Create GoogleIMG.py

---
+++ GoogleIMG.py
@@ -0,0 +1,14 @@
+import requests, json
+
+### QR ROTATE LOGIN EXAMPLE ###
+
+token_key= "INSERT TOKEN KEY HERE"
+## GET API KEY FROM https://api.boteater.us/get_token ###
+
+def search():
+    search = input("Search query: ")
+    result = json.loads(requests.get("https://api.boteater.us/googleimg?search="+search+"&auth="+token_key).text)
+    for img in result["result"]:
+        print("Link Image: "+img)
+
+search()
