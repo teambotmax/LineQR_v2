3d2b5341776451936970a34d4ee82b83599a16d0
herywinarto <hery.winarto@binus.ac.id>
2020-03-07 18:45:25 +0700

    Update Youtube-DL.py
    token api added

--- Youtube-DL.py
+++ Youtube-DL.py
@@ -1,7 +1,8 @@
 import requests, json
+token_key = ""
 
 def ytSearch(search):
-    result = json.loads(requests.get("https://api.boteater.us/ytdl?search="+search).text)
+    result = json.loads(requests.get("https://api.boteater.us/ytdl?search="+search+"&auth="+token_key).text)
     print("Title: "+result["result"]["title"])
     print("Artist: "+result["result"]["artist"])
     print("Thumbnail: "+result["result"]["thumbnail"])
@@ -9,7 +10,7 @@ def ytSearch(search):
     print("Audio Download: "+result["result"]["audio_url"])
 
 def ytLink(link):
-    result = json.loads(requests.get("https://api.boteater.us/ytdl?url="+link).text)
+    result = json.loads(requests.get("https://api.boteater.us/ytdl?url="+link+"&auth="+token_key).text)
     print("Title: "+result["result"]["title"])
     print("Artist: "+result["result"]["artist"])
     print("Thumbnail: "+result["result"]["thumbnail"])
