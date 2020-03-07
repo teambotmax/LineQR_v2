e4815f67e8747a6874e6aef653a479bd9bdf67b8
herywinarto <hery.winarto@binus.ac.id>
2020-03-07 18:44:48 +0700

    Update LineQR_V2.py
    token api added

--- LineQR_V2.py
+++ LineQR_V2.py
@@ -2,6 +2,9 @@ import requests, json
 
 ### QR ROTATE LOGIN EXAMPLE ###
 
+token_key= ""
+## GET API KEY FROM https://api.boteater.us/get_token ###
+
 header_list= ["android_lite", "chrome", "ios_ipad", "desktopmac", "desktopwin"]
 
 print(header_list)
@@ -10,15 +13,15 @@ def login():
     header = input("Insert header: ")
     if header not in header_list:
         raise Exception("Wrong header input")
-    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header).text)
+    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&auth="+token_key).text)
     print("QR Link: "+result["result"]["qr_link"])
     print("Login IP: "+result["result"]["login_ip"])
     print("QR Active For 30 Seconds")
-    result = json.loads(requests.get(result["result"]["callback"]).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Pincode: "+result["result"]["pin_code"])
-    result = json.loads(requests.get(result["result"]["callback"]).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Cert: "+result["result"]["cert"])
@@ -30,11 +33,11 @@ def loginWithCert():
     if header not in header_list:
         raise Exception("Wrong header input")
     cert = input("Insert cert: ")
-    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&cert="+cert).text)
+    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&cert="+cert+"&auth="+token_key).text)
     print("QR Link: "+result["result"]["qr_link"])
     print("Login IP: "+result["result"]["login_ip"])
     print("QR Active For 30 Seconds")
-    result = json.loads(requests.get(result["result"]["callback"]).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Cert: "+result["result"]["cert"])
@@ -46,15 +49,15 @@ def loginPushNotifSMS():
     number = input("Insert your active phone: ")
     if header not in header_list:
         raise Exception("Wrong header input")
-    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&number="+number).text)
+    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&number="+number+"&auth="+token_key).text)
     print("QR Link: "+result["result"]["qr_link"])
     print("Login IP: "+result["result"]["login_ip"])
     print("QR Active For 30 Seconds")
-    result = json.loads(requests.get(result["result"]["callback"]).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Pincode code have send to your number")
-    result = json.loads(requests.get(result["result"]["callback"]).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Cert: "+result["result"]["cert"])
