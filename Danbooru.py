b5fe49456fdb1d588ce29a785c986aeac0674200
herywinarto <hery.winarto@binus.ac.id>
2020-03-08 15:03:49 +0700

    Update Danbooru.py

--- Danbooru.py
+++ Danbooru.py
@@ -6,6 +6,9 @@ token_key= "INSERT TOKEN KEY HERE"
 ## GET API KEY FROM https://api.boteater.us/get_token ###
 
 def search():
+    if token_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+        raise Exception("Wrong API Key")
     page = input("Danboru Page: ") #example page 1 or 2 or 3 or etc
     result = json.loads(requests.get("https://api.boteater.us/danbooru?page="+page+"&auth="+token_key).text)
     for img in result["result"]:
