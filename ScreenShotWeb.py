4551d23666a75ee05b95e1e81b57036d53e92545
herywinarto <hery.winarto@binus.ac.id>
2020-03-08 15:04:11 +0700

    Update ScreenShotWeb.py

--- ScreenShotWeb.py
+++ ScreenShotWeb.py
@@ -4,6 +4,9 @@ token_key= "INSERT TOKEN KEY HERE"
 ## GET API KEY FROM https://api.boteater.us/get_token ###
 
 def search():
+    if token_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+        raise Exception("Wrong API Key")
     search = input("Link Website: ") #example google.com
     result = json.loads(requests.get("https://api.boteater.us/ssweb?url="+search+"&auth="+token_key).text)
     print("Link SS: "+result["result"])
