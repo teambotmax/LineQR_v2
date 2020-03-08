d4fc98f455ec29e4264227b4a2ecec256406662d
herywinarto <hery.winarto@binus.ac.id>
2020-03-08 15:03:33 +0700

    Update JooxMusic.py

--- JooxMusic.py
+++ JooxMusic.py
@@ -6,6 +6,9 @@ token_key= "INSERT TOKEN KEY HERE"
 ## GET API KEY FROM https://api.boteater.us/get_token ###
 
 def search():
+    if token_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+        raise Exception("Wrong API Key")
     search = input("Search query: ") #example chainsmokers
     result = json.loads(requests.get("https://api.boteater.us/joox?search="+search+"&auth="+token_key).text)
     for music in result["result"]:
