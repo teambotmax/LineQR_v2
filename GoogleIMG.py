43531a082b259f48802ef8d08016795295e5f727
herywinarto <hery.winarto@binus.ac.id>
2020-03-08 15:03:44 +0700

    Update GoogleIMG.py

--- GoogleIMG.py
+++ GoogleIMG.py
@@ -6,6 +6,9 @@ token_key= "INSERT TOKEN KEY HERE"
 ## GET API KEY FROM https://api.boteater.us/get_token ###
 
 def search():
+    if token_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+        raise Exception("Wrong API Key")
     search = input("Search query: ") # example anime
     result = json.loads(requests.get("https://api.boteater.us/googleimg?search="+search+"&auth="+token_key).text)
     for img in result["result"]:
