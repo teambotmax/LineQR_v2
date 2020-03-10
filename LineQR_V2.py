0fbb68e7bccc1801353a205f0d9ae69217cd8f86
herywinarto <hery.winarto@binus.ac.id>
2020-03-10 08:46:56 +0700

    Update LineQR_V2.py

--- LineQR_V2.py
+++ LineQR_V2.py
@@ -1,28 +1,28 @@
 import requests, json
 
-token_key= "INSERT TOKEN KEY HERE"
-## GET API KEY FROM https://api.boteater.us/get_token ###
+api_key= "INSERT API KEY HERE"
+## GET API KEY FROM LINE ID: hertot ###
 
 header_list= ["android_lite", "chrome", "ios_ipad", "desktopmac", "desktopwin"]
 
 
 def login():
-    if token_key == "INSERT TOKEN KEY HERE":
-        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+    if api_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
     print(header_list)
     header = input("Insert header: ")
     if header not in header_list:
         raise Exception("Wrong header input")
-    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&auth="+token_key).text)
+    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&auth="+api_key).text)
     print("QR Link: "+result["result"]["qr_link"])
     print("Login IP: "+result["result"]["login_ip"])
     print("QR Active For 30 Seconds")
-    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+api_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Pincode: "+result["result"]["pin_code"])
-    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+api_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Cert: "+result["result"]["cert"])
@@ -30,19 +30,19 @@ def login():
 
 
 def loginWithCert():
-    if token_key == "INSERT TOKEN KEY HERE":
-        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+    if api_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
     print(header_list)
     header = input("Insert header: ")
     if header not in header_list:
         raise Exception("Wrong header input")
     cert = input("Insert cert: ")
-    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&cert="+cert+"&auth="+token_key).text)
+    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&cert="+cert+"&auth="+api_key).text)
     print("QR Link: "+result["result"]["qr_link"])
     print("Login IP: "+result["result"]["login_ip"])
     print("QR Active For 30 Seconds")
-    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+token_key).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+api_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     print("Cert: "+result["result"]["cert"])
@@ -51,16 +51,16 @@ def loginWithCert():
     
 ### EXAMPLE ON SCRIPT BOT ###
 def getToken(to, header="ios_ipad"):
-    auth = "INPUT APIKEY HERE"
-    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&auth="+auth).text)
+    api_key = "INPUT APIKEY HERE"
+    result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&auth="+api_key).text)
     client.sendMessage(to, "QR Link: "+result["result"]["qr_link"])
     client.sendMessage(to, "Login IP: "+result["result"]["login_ip"])
     client.sendMessage(to, "QR Active For 30 Seconds")
-    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+auth).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+api_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     client.sendMessage(to, "Pincode: "+result["result"]["pin_code"])
-    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+auth).text)
+    result = json.loads(requests.get(result["result"]["callback"]+"&auth="+api_key).text)
     if result["status"] != 200:
         raise Exception("Timeout!!!")
     return result["result"]["token"]
