d359b82393e358a98e44a7ebe4ccfcf0f47fb483
herywinarto <hery.winarto@binus.ac.id>
2020-03-10 08:48:14 +0700

    Update ShorterLink.py

--- ShorterLink.py
+++ ShorterLink.py
@@ -1,15 +1,15 @@
 import requests, json
 
-token_key= "INSERT TOKEN KEY HERE"
-## GET API KEY FROM https://api.boteater.us/get_token ###
+api_key= "INSERT API KEY HERE"
+## GET API KEY FROM LINE ID: hertot ###
 
 def shorter():
-    if token_key == "INSERT TOKEN KEY HERE":
-        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+    if api_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
     url = input("Link Website: ") #example https://www.instagram.com/accounts/login/?hl=en
     data = {"url": url}
-    result = json.loads(requests.post("https://api.boteater.us/short_link"+"?auth="+token_key, data=data).text)
+    result = json.loads(requests.post("https://api.boteater.us/short_link"+"?auth="+api_key, data=data).text)
     print("Short Link: "+result["result"])
 
 shorter()
