00c15a6e082f66dd68569b10842ac84f5a8473a2
herywinarto <hery.winarto@binus.ac.id>
2020-03-10 08:48:45 +0700

    Update Wallpaper.py

--- Wallpaper.py
+++ Wallpaper.py
@@ -2,15 +2,15 @@ import requests, json
 
 ### FROM https://wall.alphacoders.com/ ###
 
-token_key= "INSERT TOKEN KEY HERE"
-## GET API KEY FROM https://api.boteater.us/get_token ###
+api_key= "INSERT API KEY HERE"
+## GET API KEY FROM LINE ID: hertot ###
 
 def search():
-    if token_key == "INSERT TOKEN KEY HERE":
-        print("GET API KEY FIRST FROM: https://api.boteater.us/get_token")
+    if api_key == "INSERT TOKEN KEY HERE":
+        print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
     search = input("Search query: ") #example naruto
-    result = json.loads(requests.get("https://api.boteater.us/alphacoders?search="+search+"&auth="+token_key).text)
+    result = json.loads(requests.get("https://api.boteater.us/alphacoders?search="+search+"&auth="+api_key).text)
     for img in result["result"]:
         print("")
         print("Image Name: "+img["name"])
