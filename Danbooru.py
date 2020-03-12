d25203df7b9d182140ea3e08e9e1175020403a91
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:01:49 +0700

    Update Danbooru.py
    ADD FAIL OVER BACKUP

--- Danbooru.py
+++ Danbooru.py
@@ -5,12 +5,23 @@ import requests, json
 api_key= "INSERT API KEY HERE"
 ## GET API KEY FROM LINE ID: hertot ###
 
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
 def search():
-    if api_key == "INSERT TOKEN KEY HERE":
+    if api_key == "INSERT API KEY HERE":
         print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
+    api_link = failOverAPI()
     page = input("Danboru Page: ") #example page 1 or 2 or 3 or etc
-    result = json.loads(requests.get("https://api.boteater.us/danbooru?page="+page+"&auth="+api_key).text)
+    result = json.loads(requests.get(api_link+"/danbooru?page="+page+"&auth="+api_key).text)
     for img in result["result"]:
         print("Link Image: "+img)
 
