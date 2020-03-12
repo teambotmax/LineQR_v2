a62858386273f9c59c827aa2f7fb70e48347d26d
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:10:40 +0700

    Update ScreenShotWeb.py
    FAIL OVER BACKUP

--- ScreenShotWeb.py
+++ ScreenShotWeb.py
@@ -3,12 +3,25 @@ import requests, json
 api_key= "INSERT API KEY HERE"
 ## GET API KEY FROM LINE ID: hertot ###
 
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
+
 def ssweb():
-    if api_key == "INSERT TOKEN KEY HERE":
+    if api_key == "INSERT API KEY HERE":
         print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
     search = input("Link Website: ") #example google.com
-    result = json.loads(requests.get("https://api.boteater.us/ssweb?url="+search+"&auth="+api_key).text)
+    result = json.loads(requests.get(failOverAPI()+"/ssweb?url="+search+"&auth="+api_key).text)
     print("Link SS: "+result["result"])
 
 ssweb()
+
+
