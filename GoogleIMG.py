675274cde0065d45adbdbac2062c53041eeb3f9c
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:09:14 +0700

    Update GoogleIMG.py

--- GoogleIMG.py
+++ GoogleIMG.py
@@ -21,9 +21,8 @@ def search():
     if api_key == "INSERT API KEY HERE":
         print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
-    api_link = failOverAPI()
     search = input("Search query: ") # example anime
-    result = json.loads(requests.get(api_link+"/googleimg?search="+search+"&auth="+api_key).text)
+    result = json.loads(requests.get(failOverAPI()+"/googleimg?search="+search+"&auth="+api_key).text)
     for img in result["result"]:
         print("Link Image: "+img)
 
