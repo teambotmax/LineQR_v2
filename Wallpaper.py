0817640dc6236ae10172d74e3629a0f0dd1440d8
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:12:21 +0700

    Update Wallpaper.py
    FAILOVER BACKUP

--- Wallpaper.py
+++ Wallpaper.py
@@ -5,12 +5,23 @@ import requests, json
 api_key= "INSERT API KEY HERE"
 ## GET API KEY FROM LINE ID: hertot ###
 
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
+
 def search():
-    if api_key == "INSERT TOKEN KEY HERE":
+    if api_key == "INSERT API KEY HERE":
         print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
     search = input("Search query: ") #example naruto
-    result = json.loads(requests.get("https://api.boteater.us/alphacoders?search="+search+"&auth="+api_key).text)
+    result = json.loads(requests.get(failOverAPI()+"/alphacoders?search="+search+"&auth="+api_key).text)
     for img in result["result"]:
         print("")
         print("Image Name: "+img["name"])
