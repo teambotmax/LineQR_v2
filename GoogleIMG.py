93a17042d5f9d6c08a930a55127ab0ecf2da6989
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:03:51 +0700

    Update GoogleIMG.py
    FAIL OVER BACKUP

--- GoogleIMG.py
+++ GoogleIMG.py
@@ -1,17 +1,29 @@
 import requests, json
 
-### FROM https://google.com ###
+### FROM https://danbooru.donmai.us/ ###
 
 api_key= "INSERT API KEY HERE"
+
 ## GET API KEY FROM LINE ID: hertot ###
 
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
 
 def search():
-    if api_key == "INSERT TOKEN KEY HERE":
+    if api_key == "INSERT API KEY HERE":
         print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
+    api_link = failOverAPI()
     search = input("Search query: ") # example anime
-    result = json.loads(requests.get("https://api.boteater.us/googleimg?search="+search+"&auth="+api_key).text)
+    result = json.loads(requests.get(api_link+"/googleimg?search="+search+"&auth="+api_key).text)
     for img in result["result"]:
         print("Link Image: "+img)
 
