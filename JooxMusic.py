0d5b8da942e097c27ed9e987a9f80d658ba1dea9
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:05:12 +0700

    Update JooxMusic.py
    ADD FAIL OVER BACKUP

--- JooxMusic.py
+++ JooxMusic.py
@@ -1,16 +1,29 @@
 import requests, json
 
-### FROM https://joox.com ###
+### FROM https://danbooru.donmai.us/ ###
 
 api_key= "INSERT API KEY HERE"
+
 ## GET API KEY FROM LINE ID: hertot ###
 
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
+
 def search():
-    if api_key == "INSERT TOKEN KEY HERE":
+    if api_key == "INSERT API KEY HERE":
         print("GET API KEY FROM LINE ID: hertot")
         raise Exception("Wrong API Key")
+    api_link = failOverAPI()
     search = input("Search query: ") #example chainsmokers
-    result = json.loads(requests.get("https://api.boteater.us/joox?search="+search+"&auth="+api_key).text)
+    result = json.loads(requests.get(api_link+"/joox?search="+search+"&auth="+api_key).text)
     for music in result["result"]:
         print("")
         print("Artis: "+music["artist"])
