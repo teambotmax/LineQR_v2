4057d7ff02634a00cea608a919c272b87a25fc14
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:13:30 +0700

    Update Youtube-DL.py
    FAILOVER BACKUP

--- Youtube-DL.py
+++ Youtube-DL.py
@@ -5,6 +5,17 @@ import requests, json
 api_key= "INSERT API KEY HERE"
 ## GET API KEY FROM LINE ID: hertot ###
 
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
+
 if api_key == "INSERT API KEY HERE":
     print("GET API KEY FROM LINE ID: hertot")
     raise Exception("Wrong API Key")
