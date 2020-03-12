a37fc7699ba1af90c77d432569dde4b46f02a7cb
herywinarto <hery.winarto@binus.ac.id>
2020-03-12 14:13:07 +0700

    Update Youtube-DL.py
    FAILOVER BACKUP

--- Youtube-DL.py
+++ Youtube-DL.py
@@ -5,12 +5,12 @@ import requests, json
 api_key= "INSERT API KEY HERE"
 ## GET API KEY FROM LINE ID: hertot ###
 
-if api_key == "INSERT TOKEN KEY HERE":
+if api_key == "INSERT API KEY HERE":
     print("GET API KEY FROM LINE ID: hertot")
     raise Exception("Wrong API Key")
 def ytSearch():
     search= input("Search query: ") #example joji cant get over you
-    result = json.loads(requests.get("https://api.boteater.us/ytdl?search="+search+"&auth="+api_key).text)
+    result = json.loads(requests.get(failOverAPI()+"/ytdl?search="+search+"&auth="+api_key).text)
     print("Title: "+result["result"]["title"])
     print("Artist: "+result["result"]["artist"])
     print("Thumbnail: "+result["result"]["thumbnail"])
@@ -19,7 +19,7 @@ def ytSearch():
 
 def ytLink():
     url= input("Link Youtube: ") #example https://www.youtube.com/watch?v=zbxAB7rTpDc
-    result = json.loads(requests.get("https://api.boteater.us/ytdl?url="+url+"&auth="+api_key).text)
+    result = json.loads(requests.get(failOverAPI()+"/ytdl?url="+url+"&auth="+api_key).text)
     print("Title: "+result["result"]["title"])
     print("Artist: "+result["result"]["artist"])
     print("Thumbnail: "+result["result"]["thumbnail"])
