638611c23438831abaf7aa33cdc2673b567a4c98
herywinarto <hery.winarto@binus.ac.id>
2020-03-13 10:47:53 +0700

    Create authkey2primary.py

---
+++ authkey2primary.py
@@ -0,0 +1,28 @@
+import requests, json
+
+### FROM https://danbooru.donmai.us/ ###
+
+api_key= "INSERT API KEY HERE"
+
+## GET API KEY FROM LINE ID: hertot ###
+def failOverAPI():
+    try:
+        result = requests.get("https://api.boteater.xyz",timeout=0.5)
+        if result.status_code == 200:
+            return "https://api.boteater.xyz"
+        else:
+            return "https://api.boteater.us"
+    except:
+        return "https://api.boteater.us"
+    
+def convert():
+    if api_key == "INSERT API KEY HERE":
+        print("GET API KEY FROM LINE ID: hertot")
+        raise Exception("Wrong API Key")
+    authkey = input("Your Authkey: ") #example page 1 or 2 or 3 or etc
+    result = json.loads(requests.get(failOverAPI()+"/authkey2primary?authkey="+authkey+"&auth="+api_key).text)
+    print("Android / IOS Token: "+ result["result"]["android_token"])
+    print("Android Lite Token: "+ result["result"]["android_lite_token"])
+        
+
+convert()
